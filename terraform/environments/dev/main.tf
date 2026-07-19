f60997ce875ad7cf61cd124a623b6af87b1a46c5
shubhamjain-tech <shubhamjain9005@outlook.com>
2026-07-19 16:06:23 +0530

    feat(terraform): add reusable security group module and eks module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -38,3 +38,29 @@ module "ecr" {
   project_name = var.project_name
   environment  = var.environment
 }
+
+module "security_group" {
+  source = "../../modules/security-groups"
+
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_id       = module.vpc.vpc_id
+}
+
+module "eks" {
+
+  source = "../../modules/eks"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  cluster_name = "${var.project_name}-${var.environment}"
+
+  kubernetes_version = "1.31"
+
+  cluster_role_arn = module.iam.eks_cluster_role_arn
+
+  private_subnet_ids = module.vpc.private_subnet_ids
+
+  eks_security_group_id = module.security_group.eks_node_security_group_id
+}
--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -9,3 +9,23 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
+
+output "alb_security_group_id" {
+  value = module.security_group.alb_security_group_id
+}
+
+output "eks_node_security_group_id" {
+  value = module.security_group.eks_node_security_group_id
+}
+
+output "database_security_group_id" {
+  value = module.security_group.database_security_group_id
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
